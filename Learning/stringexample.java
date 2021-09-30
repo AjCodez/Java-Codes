50799c296df74bbd5b3077f7f8d9eaa3fc5d28ff
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-09-30 20:03:34 +0530

    Add files via upload

---
+++ Learning/stringexample.java
@@ -0,0 +1,9 @@
+class stringexample{
+    public static void main(String[] args) {
+        String str1="Hello we are learning JAVA";
+        String str2=" Hello we are learning JAVA";
+        System.out.println(str1);
+        System.out.println("\n------------------\n");
+        System.out.println(str1.concat(str2));
+    }
+} 
