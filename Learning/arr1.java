95d74c3b1d8f209df0b9f5ca0d9715f5e265efe6
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-11-14 01:01:47 +0530

    Add files via upload

---
+++ Learning/arr1.java
@@ -0,0 +1,13 @@
+class arr{
+    public static void main(String[] args) {
+        int ar[]=new int[5];
+        ar[0]=1;
+        ar[1]=2;
+        ar[2]=3;
+        ar[3]=4;
+        ar[4]=5;
+        for(int i=0;i<ar.length;i++){
+            System.out.println(i);
+        }
+    }
+}
