12c6b4f868e05a716e516181105ceda370de8a88
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-11-14 01:02:15 +0530

    Add files via upload

---
+++ Learning/arr3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class arr{
+    public static void main(String[] args) {
+        String ar[]=new String[5];
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+        }
+    }
+}
