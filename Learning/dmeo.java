b5e4a5862243a5f49b622f69e3eba6eb43b7237d
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-11-14 01:01:32 +0530

    Add files via upload

---
+++ Learning/dmeo.java
@@ -0,0 +1,11 @@
+class dmeo
+{
+    static void func(){
+        System.out.println("GLA");
+    }
+    public static void main(String[] args) {
+        dmeo.func();
+        dmeo obj=new dmeo();
+        obj.func();
+    }
+}
